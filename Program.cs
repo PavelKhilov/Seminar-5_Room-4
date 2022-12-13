1f7753ae231929f713093e96b5c150e23babc052
Pavel <khilov.pavel@eyandex.ru>
2022-12-13 21:49:08 +0300

    Exersise 2

--- Program.cs
+++ Program.cs
@@ -29,7 +29,7 @@ int GetSumOfNegatives (int[] array)
             sum += array[i];
     return sum;
 }
-
+/*
 Console.Write("Input number elements: ");
 int lenght = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a min psible value: ");
@@ -42,3 +42,24 @@ ShowArray(myArray);
 
 int result = GetSumOfNegatives(myArray);
 Console.WriteLine($"Sum of negatives elements is {result}");
+*/
+
+// Напишите программу замены элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+int[] Reverse (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] *= -1;
+    return array;
+}
+
+Console.Write("Input number elements: ");
+int lenght = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min psible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max psible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(lenght, min, max);
+ShowArray(myArray);
+int[] newArray = Reverse(myArray);
+ShowArray(newArray);
